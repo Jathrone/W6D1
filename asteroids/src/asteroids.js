b25e964db07b64fcfcca2ee06db51e33bd2b5108
YuchengLi <jason.li.feed@gmail.com>
2019-04-15 17:26:21 -0700

    add Asteroid class

--- asteroids/src/asteroids.js
+++ asteroids/src/asteroids.js
@@ -5,7 +5,8 @@ function Asteroid(pos){
     this.color = 'red';
     this.radius = 3;
     this.vel = [3,3];
-
+    let options = {color:'red', radius:3, vel:Util.randomVec(3), pos:pos}
+    MovingObject.call(this,options)
 };
 
 Util.inherits(Asteroid,MovingObject);
